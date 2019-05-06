deae2c92af462c0090c7cb1cf7bd2ab2afffe74c
admin1 <hai2108hatayqo@gmail.com>
2019-05-06 11:13:17 +0700

    add file

---
+++ spring-internalization-blog/src/main/java/com/code/blog/request/Credential.java
@@ -0,0 +1,25 @@
+package com.code.blog.request;
+
+public class Credential {
+    private String username;
+    private String password;
+
+    public Credential() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ spring-internalization-blog/src/main/java/com/code/blog/response/User.java
@@ -0,0 +1,16 @@
+package com.code.blog.response;
+
+public class User {
+    private String userName;
+
+    public User() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
